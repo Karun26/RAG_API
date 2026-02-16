6a524539bf9e97c6aad4e652fcaf5e2e45405b3f
Sreekarun A <sreekarunis@gmail.com>
2026-02-16 13:22:42 +0530

    updated semantic test and added second doc

--- ragapi/semantic_test.py
+++ ragapi/semantic_test.py
@@ -14,6 +14,19 @@ def test_kubernetes_query():
 
     print("✅ Kubernetes query test passed")
 
+    response2 = requests.post("http://127.0.0.1:8000/query?q=Who is Karun?")
+    
+    if response2.status_code != 200:
+        raise Exception(f"Server returned {response2.status_code}: {response2.text}")
+    
+    answer2 = response2.json()["answer"]
+
+    # Check for key concepts
+    assert "AI/ML" in answer2.lower(), "Missing 'AI/ML' keyword"
+    assert "Engineer" in answer2.lower(), "Missing 'Engineer' keyword"
+
+    print("✅ Karun query test passed")
+
 if __name__ == "__main__":
     test_kubernetes_query()
     print("All semantic tests passed!")
